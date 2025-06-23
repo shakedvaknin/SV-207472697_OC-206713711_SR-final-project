40852ece126cab851c980a885cac2ccbd32a9963
COfek <ofek.cohen51@gmail.com>
2025-06-23 22:15:05 +0300

    changed div2k dataset to only downsample the images

--- Data/data_loader.py
+++ Data/data_loader.py
@@ -4,17 +4,20 @@ from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
 class DIV2KDataset(Dataset):
-    def __init__(self, hr_folder, scale=2):
+    def __init__(self, hr_folder, scale=2, hr_size=(256, 256)):
         self.hr_folder = hr_folder
         self.hr_files = sorted([f for f in os.listdir(hr_folder) if f.endswith('.png')])
         self.scale = scale
+        self.hr_size = hr_size
+        self.lr_size = (hr_size[0] // scale, hr_size[1] // scale)
+
         self.hr_transform = transforms.Compose([
-            transforms.Resize((256, 256)),   # Resize all HR images to same size
+            transforms.Resize(hr_size, interpolation=Image.BICUBIC),
             transforms.ToTensor()
         ])
+
         self.lr_transform = transforms.Compose([
-            transforms.Resize((128, 128), interpolation=Image.BICUBIC),  # Downscale
-            transforms.Resize((256, 256), interpolation=Image.BICUBIC),  # Upscale
+            transforms.Resize(self.lr_size, interpolation=Image.BICUBIC),
             transforms.ToTensor()
         ])
 
@@ -22,7 +25,10 @@ class DIV2KDataset(Dataset):
         return len(self.hr_files)
 
     def __getitem__(self, idx):
-        hr_image = Image.open(os.path.join(self.hr_folder, self.hr_files[idx])).convert('RGB')
+        hr_path = os.path.join(self.hr_folder, self.hr_files[idx])
+        hr_image = Image.open(hr_path).convert('RGB')
+
         hr_tensor = self.hr_transform(hr_image)
         lr_tensor = self.lr_transform(hr_image)
+
         return lr_tensor, hr_tensor
